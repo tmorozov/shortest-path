94360bab094471289b57e3d483db70d24d9a73c4
Timur Morozov <radist@desant.biz>
2013-08-04 20:37:15 +0000

    distance framework

--- short_path.js
+++ short_path.js
@@ -1,5 +1,17 @@
+function distance (graph, from, to) {
+  if( from === to ) {
+    return 0;
+  }
+
+}
+
 exports.calcPathsFrom = function (graph, from) {
-  return [];
+  var res = [];
+  graph.forEach(function(item, i) {
+    var dist = distance(graph, from, i);
+    res[i] = dist;
+  });
+  return res;
 };
 
 exports.showStats = function (paths) {
